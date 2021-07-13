5afb9fc55144974c8b5ed556c5ebc596635f8eb9
Ismael-Oliv <ismaelalves1@outlook.com.br>
2021-07-13 11:53:25 -0300

    First commit on feature/UserInfo

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import AppLoading from "expo-app-loading";
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+
+import { StatusBar } from "react-native";
+import { Dashboard } from "./src/screens/Dashboard";
+import theme from "./src/shared/styles/theme";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor={theme.colors.background}
+      />
+      <Dashboard />
+    </ThemeProvider>
+  );
+}
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Input } from "../../components/Input";
+
+import { Container, Title } from "./style";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Title>Pagina Dashboard</Title>
+      <Input />
+    </Container>
+  );
+}
